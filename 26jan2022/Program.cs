89998a80ddc5611f415764acbad2814a6faf38a6
Sarah Martin <blue.howrse@gmail.com>
2022-01-26 05:33:40 -0700

    Saving first draft of code

--- 26jan2022/Program.cs
+++ 26jan2022/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Section4_8Practice
+{
+    class Program
+    {
+        public interface IBusiness
+        {
+            public void BuySomething();
+            public bool SellSomething();
+            private decimal funds;
+            private string kindOfBusiness;
+            private IDictionary<string, int> QtyInventory = new IDictionary<string, int>();
+            private IDictionary<string, decimal> ProductsForSale = new IDictionary<string, decimal>();
+
+        }
+
+        public class Manufacturer : IBusiness
+        {
+            
+        }
+    }
+}
