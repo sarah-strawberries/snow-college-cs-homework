ba1dcc7ca5d0497995e10f748a1cc97b74913fe2
Sarah Martin <blue.howrse@gmail.com>
2022-01-27 22:35:36 -0700

    Practice code for section 4.10

--- 27jan2022/Program.cs
+++ 27jan2022/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+namespace ThisKeywordPractice
+{
+    class Program
+    {
+        public class Printer
+        {
+            public static void PrintMessage(string text)
+            {
+                Console.WriteLine(text);
+            }
+
+        }
+        public class NotUsefulClass
+        {
+            public string message = "This is some text. We can borrow a method from the other class to print it to the console.";
+
+            public void OkayMaybeSomewhatUseful()
+            {
+                Printer.PrintMessage(this.message);
+            }
+
+        }
+            static void Main()
+            {
+                NotUsefulClass doStuff = new NotUsefulClass();
+                doStuff.OkayMaybeSomewhatUseful();
+            }
+    }
+
+}
