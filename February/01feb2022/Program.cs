4b3837d72a4f353e531c2b4e65ff43bec76642c9
Sarah Martin <blue.howrse@gmail.com>
2022-02-01 22:29:31 -0700

    Second commit

--- February/01feb2022/Program.cs
+++ February/01feb2022/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace PracticeWithDelagates
+{
+    public delegate bool DoChores(int choreNum);
+    public class MyChores
+    {
+        public static int choreNumber;
+        public static int DoChore()
+        {
+            if (choreNumber == 0)
+            {
+                Console.WriteLine("You swept the floor!");
+                choreNumber++;
+                return choreNumber;
+            }
+            if(choreNumber == 1)
+            {
+                Console.WriteLine("You did the dishes!");
+                choreNumber++;
+                return choreNumber;
+            }
+            if (choreNumber == 2)
+            {
+                Console.WriteLine("You cleaned your room!");
+                choreNumber++;
+                return choreNumber;
+            }
+            else
+            {
+                Console.WriteLine("You washed a window!");
+                return choreNumber;
+            }
+        }
+        public int ChoreCount()
+        {
+            Console.WriteLine($"You have completed {choreNumber} chores.");
+            return 0;
+        }
+    }
+    public static Main()
+    {
+        DoChores(DoChore);
+    }
+}
