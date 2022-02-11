155b6aa2863bf1566574b1d6da578fb666ef7a52
Sarah Martin <blue.howrse@gmail.com>
2022-02-10 21:35:51 -0700

    Review day 3

---
+++ February/10feb2022/Review3.Main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static string text = "I \nam \nhaving fun \nusing escape \nsequences \nsince I just learned \nabout them \n:)";
+    static string haiku = "\n\tMy Haiku about Escape Sequences: \nEscape sequences \nLet you use more lines than one \nWhen you print some text. \n";
+    static void Main()
+    {
+    Console.WriteLine(text);
+    Console.WriteLine(haiku);
+    }
+}
