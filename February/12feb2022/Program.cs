ee6cd9f3530dcff1a1b3cb4564268f5cc0faa0f3
Sarah Martin <blue.howrse@gmail.com>
2022-02-12 23:09:04 -0700

    Halfway through

---
+++ February/12feb2022/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Contact
+{
+    string Name{get; set;}
+    ulong PhoneNumber{get
+    {} 
+    set
+    {
+        if (value !< 99999999990)
+        {throw new BadPhoneNumberExeption;
+        }
+                }
+    }
+
+    public Contact(string name, ulong number)
+    {
+        Name = name;
+        PhoneNumber = number;
+    }
+}
