e6594d81474bdf254ba4bc171be8d0339d872da8
Sarah Martin <blue.howrse@gmail.com>
2022-02-16 07:57:23 -0700

    First test passed commit; skipped writing other tests for time's sake

--- February/15feb2022/15febPracticeCode/Program.cs
+++ February/15feb2022/15febPracticeCode/Program.cs
@@ -17,8 +17,50 @@ public class MyPerson
     public static bool StoreMyPersonsInfo(MyPerson person)
     {
         //do stuff that makes the MyPerson object get saved, probably using a stream
-        // var myFile = new System.IO.StreamWriter(myFile);
-        return false;
+        System.IO.TextWriter writeText = null;
+        try 
+        {
+            writeText = new System.IO.StreamWriter(@"C:\Users\Allen\code\homeworkpractice-sarah-strawberries\February\15feb2022\myFile.txt");
+            writeText.WriteLine(person.GetType());
+            writeText.WriteLine(person.name);
+            writeText.WriteLine(person.age);
+        }
+        catch
+        {
+            return false;
+        }
+        finally 
+        {
+            if (writeText != null)
+            {
+                writeText.Close();
+            }
+        }
+        return true;
+        
+    }
+
+    public static MyPerson LoadPersonFromFile()
+    {
+        MyPerson retrievedPerson = null;
+        System.IO.TextReader readText = null;
+        try
+        {
+            readText = new System.IO.StreamReader(@"C:\Users\Allen\code\homeworkpractice-sarah-strawberries\February\15feb2022\myFile.txt");
+            string objectType = readText.ReadLine();
+            string nameText = readText.ReadLine();
+            int ageInt = int.Parse(readText.ReadLine());
+            if (objectType == "MyPerson")
+            {
+                retrievedPerson.name = nameText;
+                retrievedPerson.age = ageInt;
+            }
+        }
+        catch
+        {
+            return null;
+        }
+        return retrievedPerson;
     }
 
  public static void Main()
