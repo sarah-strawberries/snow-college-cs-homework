4c94476be0212eb0c3c8a98ba66718e6b91d7594
Sarah Martin <blue.howrse@gmail.com>
2022-02-15 22:05:52 -0700

    Finished practicing for the night, to be continued tomorrow

--- February/15feb2022/15febPracticeCode/Program.cs
+++ February/15feb2022/15febPracticeCode/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MyPerson
+{
+    public MyPerson(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public MyPerson()
+    {
+        name = "Fred";
+        age = 97;
+    }
+    string name;
+    int age;
+    public bool StoreMyPersonsInfo()
+    {
+        //do stuff that makes the MyPerson object get saved, probably using a stream
+        // var myFile = new System.IO.StreamWriter(myFile);
+        // return false;
+    }
+}
+
