a20ee60e43f207b523634eea8d279afde48cb00c
Sarah Martin <blue.howrse@gmail.com>
2022-02-15 21:54:18 -0700

    Initializing things

---
+++ February/15feb2022/15febPracticeCode.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace _15febPracticeCode.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
