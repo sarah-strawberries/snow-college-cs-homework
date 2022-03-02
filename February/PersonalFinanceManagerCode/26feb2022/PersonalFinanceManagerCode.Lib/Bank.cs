8d8ea7ca7b0536bc6042caab988de2c4e50bdcee
Sarah Martin <blue.howrse@gmail.com>
2022-03-02 08:00:23 -0700

    Practice for March 1-2, 2022

---
+++ February/PersonalFinanceManagerCode/26feb2022/PersonalFinanceManagerCode.Lib/Bank.cs
@@ -0,0 +1,14 @@
+partial namespace PersonalFinanceManager
+{
+    public class Bank
+    {
+        private string name;
+
+        public Bank(string bankName)
+        {
+            Console.WriteLine($"This bank's name is {bankName}.");
+            name == bankName;
+        }
+
+    }
+}
