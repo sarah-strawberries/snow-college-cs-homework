84bde7f45b8db674dfcd3a8440256bd4166d347a
Sarah Martin <blue.howrse@gmail.com>
2022-03-04 08:13:25 -0700

    Practice code for March 4, 2022

---
+++ 04mar2022/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections; //note to self: contains Hashtable and ArrayList
+class MyObject
+{
+    //nothing interesting in here
+}
+class Program
+{
+
+    
+    public static void Main()
+    {
+    ArrayList MyArrayList = new ArrayList();
+    MyObject obj1 = new MyObject();
+    MyObject obj2 = new MyObject();
+
+    MyArrayList.Add(obj1);
+    
+    MyArrayList.Add(obj2);
+    }
+
+}
