4336e0845c6fbc159fd06413c16683f5c0df9617
Sarah Martin <blue.howrse@gmail.com>
2022-04-08 08:07:04 -0600

    Unhandled exception with instance

--- March/Exam3Prep/Program.cs
+++ March/Exam3Prep/Program.cs
@@ -4,9 +4,24 @@ using System.Collections;
 public class Program
 {
     public Hashtable table1 = new Hashtable();
-    public void AddStuff()
+    public void AddStuffAndDoStuff()
     {
-    table1.Add("key", "value");
+        table1.Add("key", new Cow());
+        Cow retrievedCow = table1[0] as Cow;
+        retrievedCow.Moo();
     }
 
+
+    public class Cow
+    {
+        public void Moo()
+        {
+            Console.WriteLine("Moo");
+        }
+    }
+    public static void Main()
+    {
+        Program p = new Program();
+        p.AddStuffAndDoStuff();
+    }
 }
