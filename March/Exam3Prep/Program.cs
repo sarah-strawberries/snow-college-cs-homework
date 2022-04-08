453c367d3db11568abca8b8c3f54886e16a7c9a2
Sarah Martin <blue.howrse@gmail.com>
2022-04-08 07:57:53 -0600

    Pre-exam practice

---
+++ March/Exam3Prep/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+
+public class Program
+{
+    public Hashtable table1 = new Hashtable();
+    public void AddStuff()
+    {
+    table1.Add("key", "value");
+    }
+
+}
